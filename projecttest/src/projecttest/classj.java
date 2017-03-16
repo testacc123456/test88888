a68b856d347ff7f093252d6a3c8d486de0d6af89
anita <anita@anita-X550LD>
2017-03-16 12:03:29 +0100

    ccc

--- projecttest/src/projecttest/classj.java
+++ projecttest/src/projecttest/classj.java
@@ -1,7 +1,7 @@
 package projecttest;
 
 public class classj {
-//dddffffffdzzjjjccffghfgffffff
+//dddffffffdzzjjjccffghfgfffffffff
 	 public static void main(String[] args) {
 	 int i = 0;
 	    String s = ""+i;
